22c550c40f1be1927c83a2a2ac06b8db5cf546b6
VICKIE MWENDWA <mwendwavickie@gmail.com>
2024-05-02 11:48:45 +0300

    add a async function and error handling using try catch block.

--- scripts.js
+++ scripts.js
@@ -2,3 +2,15 @@ const recipeApi = ;
 const recipeUrl = ;
 // add an eventlistener to the search button
 document.getElementById('searchButton').addEventListener('click',async function(){
+    try {
+        // get the search input value
+        const searchInput = document.getElementById('searchInput').value;
+        const response = await fetch('recipeUrl'+ searchInput);
+        const data = await response.json();
+        // call the displayResults function with the fetched data
+        displayResults(data);
+    }
+    catch (error){
+        console.log('Error fetching Recipe data:',error);
+    }
+}); 
